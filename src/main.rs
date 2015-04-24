97e5b58a23453b81b284a3a7a5dd1b04caa2f567
Sergey pilyavskiy <pill.sv0@gmail.com>
2015-04-24 18:03:03 +0300

    playing around

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+enum CellState{
+    Alive,
+    Dead
 }
+
+
+struct Cell {
+    x: i8,
+    y: i8,
+    state: CellState
+}
+
+
+impl Cell {
+    fn is_alive(&self) -> bool {
+        match self.state {
+            CellState::Alive => true,
+            CellState::Dead => false
+        }
+    }
+
+
+}
+
+fn print_cell_data (cell: &Cell) {
+    println!(
+        "x: {x}, y: {y}, state: {state}", 
+        x=cell.x,
+        y=cell.y,
+        state = match cell.state {
+            CellState::Dead => "dead",
+            CellState::Alive => "alive"
+        }
+    );
+}
+
+fn main() {
+    println!("Hello");
+    let height = 10;
+    let width = height;
+
+    for y in 0..height {
+        for x in 0..width {
+            print_cell_data(&Cell{x: x, y: y, state: CellState::Alive});
+        }
+    }
+    let cell = Cell{x: 0, y: 0, state: CellState::Alive};
+    match cell {
+        Cell {state: CellState::Alive, ..} => println!("cell is alive"),
+        Cell {state: CellState::Dead, ..} => println!("cell is dead")
+    }  
+
+    if cell.is_alive() {
+        println!("We determined that cell is alive through 'is_alive' method");
+    }
+}
+
